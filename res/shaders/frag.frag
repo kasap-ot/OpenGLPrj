5161f83ca8c7e5575d5b9af4a6c57461639f77ae
Kasap.ot <kasapinov.petar@gmail.com>
2020-09-05 23:32:59 +0200

    Add Shader class

--- res/shaders/frag.frag
+++ res/shaders/frag.frag
@@ -1,1 +1,10 @@
-#version 330
+#version 400 core
+
+in vec3 color;
+
+out vec4 fragColor;
+
+void main() 
+{ 
+    fragColor = vec4(color, 1.0);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "OpenGLPrj.hpp"
+#include "Shader.h"
 #include <GLFW/glfw3.h>
 #include <cstdlib>
 #include <iostream>
@@ -6,20 +7,6 @@ using namespace std;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-
-const char* vertexShaderSource = 
-    "#version 400 core\n"
-    "layout(location = 0) in vec3 aPos;\n"
-    "void main() { \n"
-        "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
-    "}\0";
-
-const char* fragmentShaderSource =
-    "#version 400 core\n"
-    "out vec4 fragColor;\n"
-    "void main() { \n"
-        "fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0);\n"
-    "}\0";
 
 int main(int argc, char* argv[])
 {
@@ -47,10 +34,10 @@ int main(int argc, char* argv[])
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     float verticies[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.5f,  0.5f, 0.0f,
-        -0.5f,  0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,
+         0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,
+        -0.5f,  0.5f, 0.0f,     1.0f, 1.0f, 1.0f
     };
 
     unsigned int indices[] = {
@@ -66,52 +53,19 @@ int main(int argc, char* argv[])
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     unsigned int EBO;
     glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
-    int success;      
-    char infoLog[512];
-
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        cout << "ERROR::SHADER::VERTEX::COMPILATION_FAIL" << endl;
-        cout << infoLog << endl; }
-
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAIL" << endl;
-        cout << infoLog << endl; }
-
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        cout << "ERROR::SHADER_PROGRAM::LINK_FAIL" << endl;
-        cout << infoLog << endl; }
-
-    glUseProgram(shaderProgram);
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    Shader shader = Shader("C:/Users/petar/OpenGLPrj/res/shaders/vert.vert", 
+                           "C:/Users/petar/OpenGLPrj/res/shaders/frag.frag");   // Absolute path. Change appropriately
+    shader.use();
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
@@ -119,7 +73,7 @@ int main(int argc, char* argv[])
     {
         processInput(window);
 
-        glClearColor(0.3f, 0.2f, 0.1f, 1.0f);
+        glClearColor(0.6f, 0.6f, 0.6f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -128,7 +82,6 @@ int main(int argc, char* argv[])
         glfwPollEvents();
     }
 
-    glDeleteProgram(shaderProgram);
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
 
@@ -137,7 +90,7 @@ int main(int argc, char* argv[])
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(50, 50, width, height);
+    glViewport(0, 0, width, height);
 }
 
 void processInput(GLFWwindow* window)
